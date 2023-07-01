1f848ec79627ed6151992850d5b2dfb4320a2b81
Ruchir-Bhatia <bhatiaruchir04@gmail.com>
2023-07-01 12:48:59 +0530

    change to hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,8 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -19,30 +21,32 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Ruchir Bhatia</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a Web Developer and a Learner.<br className='sm:block hidden' />
+            I am a Web Developer and a Learner.
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
+      {isMobile && (
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+          <a href='#about'>
+            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className='w-3 h-3 rounded-full bg-secondary mb-1'
+              />
+            </div>
+          </a>
+        </div>
+      )}
     </section>
   );
 };
